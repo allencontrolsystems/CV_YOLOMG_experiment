7c1f84e172f86c1dd4c7ba4cfbfeb55aa8865e66
Michael Cui <michael.cui@allencontrolsystems.com>
2025-05-13 15:34:45 -0500

    evaluation setup on the lambda machine

--- test_code/FD3_mask.py
+++ test_code/FD3_mask.py
@@ -8,7 +8,7 @@ import imgviz
 kernel_size = 3
 
 
-def FD3_mask(lastFrame1, lastFrame2, currentFrame, video_name, frame_count):
+def FD3_mask(lastFrame1, lastFrame2, currentFrame, video_name, frame_count, save_path=None):
     lastFrame1 = cv2.GaussianBlur(lastFrame1, (11, 11), 0)
     lastFrame1 = cv2.cvtColor(lastFrame1, cv2.COLOR_BGR2GRAY)
 
@@ -73,8 +73,9 @@ def FD3_mask(lastFrame1, lastFrame2, currentFrame, video_name, frame_count):
     # if not os.path.exists(save_path):
     #     os.makedirs(save_path)
     # cv2.imwrite(save_path + '/' + video_name + '_' + str(frame_count).zfill(4) + '.jpg', frameDiff)
-
-    save_path = '/home/acs/YOLOMG/full_data/phantom-dataset/mask31/' + video_name
+    
+    if save_path is None:
+        save_path = '/home/acs/YOLOMG/full_data/phantom-dataset/mask31/' + video_name
 
     if not os.path.exists(save_path):
         os.makedirs(save_path)
